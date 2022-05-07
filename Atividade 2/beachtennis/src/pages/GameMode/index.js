50319d86889060a4a5a48c950f92bcc992196ff5
Gabriel Freire <gabrielfvale@gmail.com>
2022-05-07 16:56:22 -0300

    Start scoreboard component

--- Atividade 2/beachtennis/src/pages/GameMode/index.js
+++ Atividade 2/beachtennis/src/pages/GameMode/index.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
+
 import ControlPanel from "components/ControlPanel";
 import PageContainer from "components/PageContainer";
+import Scoreboard from "components/Scoreboard";
 
 import { useMatch } from "providers/MatchProvider";
 
 const GameMode = () => {
   const { matchSets, superTieBreak } = useMatch();
 
+  const [gameTime, setGameTime] = useState(0);
+
+  const parseTime = (seconds) =>
+    new Date(seconds * 1000).toISOString().substr(11, 8);
+
+  useEffect(() => {
+    const interval = setInterval(() => setGameTime((prev) => prev + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <PageContainer>
       <ControlPanel matchSets={matchSets} superTieBreak={superTieBreak} />
+      <span>{parseTime(gameTime)}</span>
+      <Scoreboard />
     </PageContainer>
   );
 };
